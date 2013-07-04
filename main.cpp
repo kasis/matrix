77291b292f34a6c9d36b7c36cdf3debd77e95ef2
kasis <sasha.melkonyan@gmail.com>
2013-07-04 15:45:25 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "lib/matrix.h"
+#include <vector>
+using namespace std;
+int f(int a) {return (a+1)/2;}
+int main()
+{
+    try
+    {
+        int n=5;
+        Matrix<int> a(5,5,EYE);
+        cout << a+a*a;
+        cout << a.eachEqual(&f);
+    }
+    catch (string err)
+    {
+        cout << err << endl;
+    }
+    //cout << b(0,0) << endl;
+    return 0;
+}
